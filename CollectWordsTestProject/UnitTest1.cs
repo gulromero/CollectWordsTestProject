861316d66fc81cb403a3cc01c73a89cc056ff978
akaics <guls0489@edu.zealand.dk>
2024-03-13 09:37:44 +0100

    Add project files.

---
+++ CollectWordsTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CollectWordsTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            private static readonly string DriverDirectory = "\"C:\\Driver\\chromedriver.exe\";
+            private static IWebDriver _driver;
+
+        [ClassInitialize]
+
+        }   
+    }
+}

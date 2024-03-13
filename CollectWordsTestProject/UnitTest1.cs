618ad14018f3984f4ec83138e303437290b08b34
akaics <guls0489@edu.zealand.dk>
2024-03-13 10:12:09 +0100

    FIRST TEST WORKS <3

--- CollectWordsTestProject/UnitTest1.cs
+++ CollectWordsTestProject/UnitTest1.cs
@@ -1,16 +1,41 @@
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+
 namespace CollectWordsTestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        private static readonly string DriverDirectory = "C:\\Driver";
+        private static IWebDriver _driver;
+
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _driver = new ChromeDriver(DriverDirectory);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
-            private static readonly string DriverDirectory = "\"C:\\Driver\\chromedriver.exe\";
-            private static IWebDriver _driver;
+            _driver.Navigate().GoToUrl("https://vuecollectwordsgulsum.azurewebsites.net/");   
+            Assert.AreEqual("Collect words", _driver.Title);
+        }
 
-        [ClassInitialize]
 
-        }   
     }
 }
